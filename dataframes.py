6a81aad798969c858d048bc9b0abbcbbf1849979
isrt09 <mazedur@gmail.com>
2021-06-05 00:29:48 +0600

    Rows by Index Label

--- dataframes.py
+++ dataframes.py
@@ -3,3 +3,15 @@ import pandas as pd
 data = pd.read_csv("jamesbond.csv")
 data.set_index("Film", inplace=True)
 data.reset_index(drop=False, inplace=True)
+
+
+data = pd.read_csv("jamesbond.csv", index_col="Film")
+data.sort_index(inplace=True)
+data.head()
+data.loc["A View to a Kill"]
+data.loc["GoldenEye"]
+data.loc["Casino Royale"]
+data.loc["Casino Royale" : "Moonraker"]
+data.loc["CasinoRoyale " : ]
+data.loc[["Casino Royale","Moonraker","Die Another Day"]]
+"Casino Royale" in data.index
